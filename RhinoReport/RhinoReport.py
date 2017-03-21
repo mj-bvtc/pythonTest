b82c124e29dd4f1e5597475f9d768b7caec3f6e1
Matthew K <mkreidler@bostonvalley.com>
2017-03-21 15:56:43 -0400

    Fixed bugs in the rhino reporter, added summary section

--- RhinoReport/RhinoReport.py
+++ RhinoReport/RhinoReport.py
@@ -1,36 +1,71 @@
 import os
 import collections
 import HiddenFiles
-
-target = r"V:\Projects\Miscericordia University\_Field Work Folder Template (Do Not Delete)\Agi-Renders"
-count_global = collections.Counter()        # built-in counting object, used globally
-print(f"This is the target path that will be walked: {target}\n")
+import datetime
 
 
-# walk the tree of the specified target path
-for root, dirs, files in os.walk(target):
+def rhino_report(target):
+    """
+    Walks through provided target path and reports
+    on the quantities of different file types
 
-    # remove hidden files, .psx extensions, and directories that end in .files
-    files[:] = [f for f in files if not HiddenFiles.is_hidden(os.path.join(root, f))
-                or f.endswith(".psx")]
-    dirs[:] = [d for d in dirs if not d.endswith(".files")]
+    parameter: target
 
-    # only concerned with areas that contain files
-    if files:
-        count = collections.Counter()   # instantiate local counter
-        new_root = root.replace(target+"\\", "")
-        print(f"Relative path {new_root} contains: ")
+    returns: None
+    """
 
-        # search and count file extensions
-        for file in files:
-            name, ext = os.path.splitext(file)
-            count[ext] += 1         # increment local count
-            count_global[ext] += 1  # increment global count
-            # print(file)            # if interested, uncomment this to see files
-        print(count)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    report_name = "RhinoReport_" + timestamp + ".txt"
+    report_path = os.path.join(target, report_name)
+    report = open(report_path, "w")
+    report.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n")
+    dir_count = 0
+    found_rhino = 0
+    count_global = collections.Counter()  # built-in counting object, used globally
+    print(f"Report for path: {target}\n", file=report)
 
-    print("\n")                     # create a visual space between directories
+    # walk the tree of the specified target path
+    for root, dirs, files in os.walk(target):
 
-# print global counts
-print("Global counts: ")
-print(count_global)
+        # remove hidden files, .psx extensions, and directories that end in .files
+        files[:] = [f for f in files if not (HiddenFiles.is_hidden(os.path.join(root, f)))]
+        files[:] = [f for f in files if not (f.endswith(".psx") or
+                    f.endswith(".3dmbak") or f.endswith(".txt"))]
+        dirs[:] = [d for d in dirs if not d.endswith(".files")]
+
+        # only concerned with areas that contain files
+
+        if files:
+            dir_count += 1
+            count = collections.Counter()   # instantiate local counter
+            new_root = root.replace(target+"\\", "")
+            print(f"{new_root} contains: ", file=report)
+
+            # search and count file extensions
+            for file in files:
+                name, ext = os.path.splitext(file)
+                if ext.upper() == ".3DM":
+                    found_rhino += 1
+                count[ext] += 1         # increment local count
+                count_global[ext] += 1  # increment global count
+                # print(file)            # if interested, uncomment this to see files
+            print(count, file=report)
+
+        print("\n", file=report)                     # create a visual space between directories
+
+    # print global counts
+    print("\n***\n", file=report)
+    print("Global counts: ", file=report)
+    print(count_global, file=report)
+    missing_rhino = dir_count - found_rhino
+    print(f"\n\nSummary:\nNumber of Directories: {dir_count}\n"
+          f"Number of missing Rhino files: {missing_rhino}", file=report)
+    report.close()
+
+
+def main():
+    target = r"V:\Projects\Miscericordia University\_Field Work Folder Template (Do Not Delete)\Agi-Renders"
+    rhino_report(target)
+
+if __name__ == "__main__":
+    main()
