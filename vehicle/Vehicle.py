ba48112352f3ed5a2a59cac813d9f426e40b9b62
Matthew K <mkreidler@bostonvalley.com>
2017-03-22 14:25:08 -0400

    Adding inheritance to vehicle class

--- vehicle/Vehicle.py
+++ vehicle/Vehicle.py
@@ -1,9 +1,13 @@
-class Vehicle:
+import uuid
+
+
+class Vehicle(object):
     """
     A mechanical transportation
     method
     """
     def __init__(self, type, name):
+        self.id = uuid.uuid4()
         self.type = type
         self.name = name
         self.power = None
@@ -11,9 +15,33 @@ class Vehicle:
         self.passengers = None
 
 
+# TODO inherit vehicle into a child class
+class Truck(Vehicle):
+    """
+    Vehicle that performs work and transports
+    passengers
+    """
+
+    def __init__(self, name):
+        Vehicle.__init__(self, "Truck", name)
+        self.is_4x4 = None
+        self.bed_area = 32
+
+
+def hello(name):
+    print(f"Hello {name}!")
+    return
+
 if __name__ == "__main__":
     a = Vehicle("truck", "Berty")
     print(a.name)
+    b = Truck("ben")
+    msg1 = (f"{b.name.title()} is a {b.type.lower()} "
+            f"with a bed area of {b.bed_area} square feet")
+    msg2 = (f"{b.name.title()}'s id is {b.id}")
+
+    print(msg1, len(msg1))
+    print(msg2, len(msg2))
 
 
 
