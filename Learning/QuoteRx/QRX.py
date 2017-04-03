66e2345fec7fc68c270051d614027aa57d554560
Matthew K <mkreidler@bostonvalley.com>
2017-04-03 11:01:01 -0400

    Learning and testing qrx and datascience libraries

---
+++ Learning/QuoteRx/QRX.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+import importlib
+import os
+
+
+
+
+
+csv = r"C:\Users\mkreidler\Desktop\TEST.csv"
+df = pd.read_csv(csv)
+#print(list(df.columns.values))
+print(df.columns)
