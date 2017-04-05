d6177d159c91be52043a3c853a5411cf30208331
Matthew K <mkreidler@bostonvalley.com>
2017-04-05 17:59:39 -0400

    Working on bvtc framework

--- Learning/BVTC/block.py
+++ Learning/BVTC/block.py
@@ -1,5 +1,11 @@
 import common
 
+block_dict = {
+    "Band": "B",
+    "Bracket": "BK",
+    "Ashlar": "A",
+    "Watertable": "W"
+}
 
 class Block(common.Common):
     """A single unit/product of masonry"""
@@ -7,5 +13,29 @@ class Block(common.Common):
         super().__init__()
         self.project = None
         self.status = None
-        self.block_type = None
+        self.style = None
         self.location = None
+        self.quantity = None
+        self.style_number = None
+        self.orientation = None
+        self.estimating_id = None
+        self.region = None
+        self.ornament = None
+        self.form_method = None
+        self.sample = None
+
+    @property
+    def block_id(self):
+        prefix = block_dict[self.style]
+        suffix = str(self.style_number)
+        return prefix + suffix
+
+
+def main():
+    b = Block()
+    b.style = "Watertable"
+    b.style_number = 12
+    print(b.block_id)
+
+if __name__ == "__main__":
+    main()
