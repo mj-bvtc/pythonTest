bf677c79fc4ec52eb302f550ddc465732179b48b
Matthew K <mkreidler@bostonvalley.com>
2017-07-10 16:17:20 -0400

    taking the text and point location from rhino dot into grasshopper

---
+++ Rhino/Dot_pointText.py
@@ -0,0 +1,15 @@
+import rhinoscriptsyntax as rs
+import Rhino
+import Rhino.Geometry as rg
+
+doc = Rhino.RhinoDoc.ActiveDoc
+
+dot_guids = rs.GetObjects()
+
+for id in dot_guids:
+    geo = doc.Objects.Find(id)
+    print geo.Geometry.Point
+    print geo.Geometry.Text
+    print geo.Geometry.Text.Size
+
+
